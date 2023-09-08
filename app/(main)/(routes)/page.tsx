76f06c3d1ab725bc972428d47daf8118f0662170
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-09-08 10:36:40 +0000

    add UserButton component to check if auth works

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div className="flex flex-col w-full">
       <h1>Discord Clone</h1>
+      <UserButton />
     </div>
   );
 }
