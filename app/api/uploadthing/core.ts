d45d93955f8c0de52c2f5a02eeb806b4ea4f0ee4
Zaid-maker <pzhafeez@gmail.com>
2023-09-10 21:16:20 +0500

    format

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -8,17 +8,16 @@ const handleAuth = () => {
 
     if (!userId) throw new Error("Unauthorized")
 
-    return { userId: userId }
+    return { userId: userId };
 }
 
 export const ourFileRouter = {
-    serverImage: f({
-        image: {
-            maxFileSize: "4MB",
-            maxFileCount: 1
-        }
-    }).middleware(() => handleAuth()).onUploadComplete(() => { }),
-    messageFile: f(["image", "pdf"]).middleware(() => handleAuth()).onUploadComplete(() => { })
+    serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+        .middleware(() => handleAuth())
+        .onUploadComplete(() => { }),
+    messageFile: f(["image", "pdf"])
+        .middleware(() => handleAuth())
+        .onUploadComplete(() => { })
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
