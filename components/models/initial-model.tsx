0a62dd1be049653eb4c0f52a7cf2893acb1db2e4
Zaid-maker <pzhafeez@gmail.com>
2023-09-10 21:26:27 +0500

    add loading state on input

--- components/models/initial-model.tsx
+++ components/models/initial-model.tsx
@@ -49,6 +49,8 @@ export const InitialModel = () => {
     },
   });
 
+  const isLoading = form.formState.isSubmitting;
+
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     console.log(values);
   };
@@ -60,13 +62,13 @@ export const InitialModel = () => {
   return (
     <Dialog open>
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
-        <DialogHeader className="pt-0 px-6">
+        <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Customize your Server
+            Customize your server
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500">
-            Give your server a personality with a name and image. You can always
-            change it later
+            Give your server a personality with a name and an image. You can
+            always change it later.
           </DialogDescription>
         </DialogHeader>
         <Form {...form}>
@@ -100,6 +102,7 @@ export const InitialModel = () => {
                     </FormLabel>
                     <FormControl>
                       <Input
+                        disabled={isLoading}
                         className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
                         placeholder="Enter Server Name"
                         {...field}
@@ -111,7 +114,9 @@ export const InitialModel = () => {
               />
             </div>
             <DialogFooter className="bg-gray-100 px-6 py-4">
-              <Button variant="primary">Create Server</Button>
+              <Button variant="primary" disabled={isLoading}>
+                Create Server
+              </Button>
             </DialogFooter>
           </form>
         </Form>
