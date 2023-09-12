17027ae0bde635b09abee7e9c5fd0d3307e57832
Zaid-maker <pzhafeez@gmail.com>
2023-09-12 20:30:47 +0500

    add NavigationSidebar component

--- app/(main)/(routes)/layout.tsx
+++ app/(main)/(routes)/layout.tsx
@@ -1,9 +1,12 @@
+import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
 import React from "react";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
-      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0"></div>
+      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+        <NavigationSidebar />
+      </div>
       <main className="md:pl-[72px] h-full">{children}</main>
     </div>
   );
---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,28 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import React from "react";
+
+export const NavigationSidebar = async () => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/");
+  }
+
+  const servers = await db.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  return (
+    <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] bg-[#E3E5E8] py-3">
+      NavigationSidebar
+    </div>
+  );
+};
