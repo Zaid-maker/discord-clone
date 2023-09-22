436720c72948ef93e2a09ba416997d19ad630511
DevMirza <pzhafeez@gmail.com>
2023-09-22 15:18:57 +0000

    add API to Change memvers role

---
+++ app/api/members/[memberId]/route.ts
@@ -0,0 +1,65 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { memberId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { searchParams } = new URL(req.url);
+    const { role } = await req.json();
+
+    const serverId = searchParams.get("serverId");
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!serverId) {
+      throw new NextResponse("Server ID Missing", { status: 400 });
+    }
+
+    if (!params.memberId) {
+      throw new NextResponse("Member ID Missing", { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        profileId: profile.id,
+      },
+      data: {
+        members: {
+          update: {
+            where: {
+              id: params.memberId,
+              profileId: {
+                not: profile.id,
+              },
+            },
+            data: {
+              role,
+            },
+          },
+        },
+      },
+      include: {
+        members: {
+          include: {
+            profile: true,
+          },
+          orderBy: {
+            role: "asc",
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[MEMBERS_ID_PATCH]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
