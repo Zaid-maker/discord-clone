cac1bed686d3d007efc08d0c021aa50187f2cb97
DevMirza <pzhafeez@gmail.com>
2023-09-22 15:24:44 +0000

    add API to kick users

--- app/api/members/[memberId]/route.ts
+++ app/api/members/[memberId]/route.ts
@@ -2,6 +2,62 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+export async function DELETE(
+  req: Request,
+  { params }: { params: { memberId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { searchParams } = new URL(req.url);
+
+    const serverId = searchParams.get("serverId");
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!serverId) {
+      throw new NextResponse("Server ID Missing", { status: 400 });
+    }
+
+    if (!params.memberId) {
+      throw new NextResponse("Member ID Missing", { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        profileId: profile.id,
+      },
+      data: {
+        members: {
+          deleteMany: {
+            id: params.memberId,
+            profileId: {
+              not: profile.id,
+            },
+          },
+        },
+      },
+      include: {
+        members: {
+          include: {
+            profile: true,
+          },
+          orderBy: {
+            role: "asc",
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[MEMBERS_ID_DELETE]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
 export async function PATCH(
   req: Request,
   { params }: { params: { memberId: string } }
