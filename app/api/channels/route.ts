e35d827ffc8da03b4fb841a68f5d9ab8882dad73
DevMirza <pzhafeez@gmail.com>
2023-09-23 09:52:36 +0000

    add API to create channels

---
+++ app/api/channels/route.ts
@@ -0,0 +1,58 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { MemberRole } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    console.log("request recieved to create channel on the server!");
+
+    const profile = await currentProfile();
+    const { name, type } = await req.json();
+    const { searchParams } = new URL(req.url);
+
+    const serverId = searchParams.get("serverId");
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!serverId) {
+      return new NextResponse("Server ID Missing", { status: 400 });
+    }
+
+    if (name === "gemeral") {
+      return new NextResponse("Channel name cannot be 'general'", {
+        status: 401,
+      });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+            },
+          },
+        },
+      },
+      data: {
+        channels: {
+          create: {
+            profileId: profile.id,
+            name,
+            type,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("CHANNELS_POST", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
