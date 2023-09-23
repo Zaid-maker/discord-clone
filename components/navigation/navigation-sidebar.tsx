04d88469a2d07a3b61c68bc6e90012ed03407924
DevMirza <pzhafeez@gmail.com>
2023-09-23 09:59:52 +0000

    fix navigation sidebar

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -32,7 +32,7 @@ export const NavigationSidebar = async () => {
       <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
       <ScrollArea className="flex-1 w-full">
         {servers.map((server) => (
-          <div key={server.id}>
+          <div key={server.id} className="mb-4">
             <NavigationItem
               id={server.id}
               name={server.name}
