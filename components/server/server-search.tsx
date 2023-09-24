f23eff564eeaac8fa90027c54670af07ae5cd5d6
Zaid-maker <pzhafeez@gmail.com>
2023-09-24 21:25:10 +0500

    update

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -25,6 +25,9 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
         <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
           Search
         </p>
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[18px] font-medium text-muted-foreground ml-auto">
+          <span className="text-xs">CMD</span>K
+        </kbd>
       </button>
     </>
   );
