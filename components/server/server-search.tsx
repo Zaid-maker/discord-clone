c06ca60a80f6d65e74f1b0e55a3a04d1743146fb
Zaid-maker <pzhafeez@gmail.com>
2023-09-24 20:50:57 +0500

    add search button on ServerSearch component

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Search } from "lucide-react";
 import React from "react";
 
 interface ServerSearchProps {
@@ -17,5 +18,14 @@ interface ServerSearchProps {
 }
 
 export const ServerSearch = ({ data }: ServerSearchProps) => {
-  return <div>ServerSearch</div>;
+  return (
+    <>
+      <button className="group px-2 py- rounde flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+        <Search className="w-4 h-4 text-zinc-500 dark:text-zinc-400" />
+        <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
+          Search
+        </p>
+      </button>
+    </>
+  );
 };
