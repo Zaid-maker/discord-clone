555a7bcab57887ea66dec9a578bdad2c89dbedf4
Zaid-maker <pzhafeez@gmail.com>
2023-09-24 21:36:21 +0500

    fix the button

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -20,13 +20,16 @@ interface ServerSearchProps {
 export const ServerSearch = ({ data }: ServerSearchProps) => {
   return (
     <>
-      <button className="group px-2 py- rounde flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+      <button
+        onClick={() => {}}
+        className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+      >
         <Search className="w-4 h-4 text-zinc-500 dark:text-zinc-400" />
         <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
           Search
         </p>
-        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[18px] font-medium text-muted-foreground ml-auto">
-          <span className="text-xs">CMD</span>K
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto">
+          <span className="text-xs">⌘</span>K
         </kbd>
       </button>
     </>
