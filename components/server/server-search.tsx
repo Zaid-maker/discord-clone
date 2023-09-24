276f847002930d48ba1de0a59e0b70cb74b7fb6e
Zaid-maker <pzhafeez@gmail.com>
2023-09-24 22:04:39 +0500

    add useEffect to make ctrl + k to work

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Search } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   CommandDialog,
   CommandEmpty,
@@ -31,6 +31,19 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
   const params = useParams();
 
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", down);
+
+    return () => document.removeEventListener("keydown", down);
+  }, []);
 
   const onClick = ({
     id,
