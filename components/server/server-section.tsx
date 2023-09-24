482fe6355458945a1706999a2e95ace2e574fa40
Zaid-maker <pzhafeez@gmail.com>
2023-09-24 23:29:18 +0500

    add a label etc

--- components/server/server-section.tsx
+++ components/server/server-section.tsx
@@ -2,6 +2,8 @@
 
 import { ServerWithMembersWithProfiles } from "@/types";
 import { ChannelType, MemberRole } from "@prisma/client";
+import { ActionToolTip } from "../action-tooltip";
+import { Plus } from "lucide-react";
 
 interface ServerSectionProps {
   label: string;
@@ -18,5 +20,18 @@ export const ServerSection = ({
   channelType,
   server,
 }: ServerSectionProps) => {
-  return <div>ServerSection</div>;
+  return (
+    <div className="flex items-center justify-between py-2">
+      <p className="text-xs uppercase font-semibold text-zinc-500 dark:text-zinc-400">
+        {label}
+      </p>
+      {role !== MemberRole.GUEST && sectionType === "channels" && (
+        <ActionToolTip label="Create Channels" side="top">
+          <button className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition">
+            <Plus className="h-4 w-4" />
+          </button>
+        </ActionToolTip>
+      )}
+    </div>
+  );
 };
