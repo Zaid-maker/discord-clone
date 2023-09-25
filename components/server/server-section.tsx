f6b6602add5d3d86b2540ebf1bd1ff6b20d0f485
Zaid-maker <pzhafeez@gmail.com>
2023-09-25 15:46:11 +0500

    add a onClick to create channel

--- components/server/server-section.tsx
+++ components/server/server-section.tsx
@@ -3,7 +3,8 @@
 import { ServerWithMembersWithProfiles } from "@/types";
 import { ChannelType, MemberRole } from "@prisma/client";
 import { ActionToolTip } from "../action-tooltip";
-import { Plus } from "lucide-react";
+import { Plus, Settings } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerSectionProps {
   label: string;
@@ -20,6 +21,8 @@ export const ServerSection = ({
   channelType,
   server,
 }: ServerSectionProps) => {
+  const { onOpen } = useModal();
+
   return (
     <div className="flex items-center justify-between py-2">
       <p className="text-xs uppercase font-semibold text-zinc-500 dark:text-zinc-400">
@@ -27,8 +30,21 @@ export const ServerSection = ({
       </p>
       {role !== MemberRole.GUEST && sectionType === "channels" && (
         <ActionToolTip label="Create Channels" side="top">
-          <button className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition">
+          <button
+            onClick={() => onOpen("createChannel")}
+            className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+          >
             <Plus className="h-4 w-4" />
+          </button>
+        </ActionToolTip>
+      )}
+      {role === MemberRole.ADMIN && sectionType === "members" && (
+        <ActionToolTip label="Create Channels" side="top">
+          <button
+            onClick={() => onOpen("members", { server })}
+            className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+          >
+            <Settings className="h-4 w-4" />
           </button>
         </ActionToolTip>
       )}
