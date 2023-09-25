194f78ff82f5731a89424a0b1abcad952bf18c24
Zaid-maker <pzhafeez@gmail.com>
2023-09-25 22:14:20 +0500

    update the component

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import { Channel, MemberRole, Server } from "@prisma/client";
+import { cn } from "@/lib/utils";
+import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
+import { Edit, Hash, Mic, Trash, Video } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { ActionToolTip } from "../action-tooltip";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -8,10 +12,49 @@ interface ServerChannelProps {
   role?: MemberRole;
 }
 
+const iconMap = {
+  [ChannelType.TEXT]: Hash,
+  [ChannelType.AUDIO]: Mic,
+  [ChannelType.VIDEO]: Video,
+};
+
 export const ServerChannel = ({
   channel,
   server,
   role,
 }: ServerChannelProps) => {
-  return <div>ServerChannel</div>;
+  const params = useParams();
+  const router = useRouter();
+
+  const Icon = iconMap[channel.type];
+
+  return (
+    <button
+      onClick={() => {}}
+      className={cn(
+        "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
+        params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
+      )}
+    >
+      <Icon className="flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400" />
+      <p
+        className={cn(
+          "line-clamp-1 font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
+          params?.channelId === channel.id &&
+            "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+        )}
+      >
+        {channel.name !== "general" && role !== MemberRole.GUEST && (
+          <div className="ml-auto flex items-center gap-x-2">
+            <ActionToolTip label="Edit">
+              <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+            </ActionToolTip>
+            <ActionToolTip label="Edit">
+              <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+            </ActionToolTip>
+          </div>
+        )}
+      </p>
+    </button>
+  );
 };
