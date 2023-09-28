11a9d20c672800249d95bb2befe14664ec3622f8
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-09-28 16:44:45 +0000

    fix onClick when clicking channels

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
-export const ChannelIdPage = () => {
+const ChannelIdPage = () => {
   return <div>ChannelIdPage</div>;
 };
+
+export default ChannelIdPage;
--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -32,7 +32,7 @@ export const ServerChannel = ({
   const Icon = iconMap[channel.type];
 
   const onClick = () => {
-    router.push(`servers/${params?.serverId}/channels/${channel.id}`);
+    router.push(`/servers/${params?.serverId}/channels/${channel.id}`);
   };
 
   const onAction = (e: React.MouseEvent, action: ModalType) => {
