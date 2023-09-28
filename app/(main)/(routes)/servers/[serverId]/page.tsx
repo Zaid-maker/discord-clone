739f2a3c748e325cee9fae9bc3ebe71d4ffd3cac
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-09-28 16:58:23 +0000

    update ServerIdPage

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,50 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const ServerIdPage = () => {
-  return <div>ServerIdPage</div>;
+interface ServerIdProps {
+  params: {
+    serverId: string;
+  };
+}
+
+const ServerIdPage = async ({ params }: ServerIdProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const InitialChannel = server?.channels[0];
+
+  if (InitialChannel?.name !== "general") {
+    return null;
+  }
+
+  return redirect(`/servers/${params.serverId}/channels/${InitialChannel?.id}`);
 };
 
 export default ServerIdPage;
