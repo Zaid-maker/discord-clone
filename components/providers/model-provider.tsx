429223f093c83b7fe96de14c14d71f190b72a390
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-09-28 11:09:59 +0000

    add the modal to ModalProvider

--- components/providers/model-provider.tsx
+++ components/providers/model-provider.tsx
@@ -9,6 +9,7 @@ import { CreateChannelModal } from "@/components/models/create-channel-modal";
 import { LeaveServerModal } from "@/components/models/leave-server-modal";
 import { DeleteServerModal } from "@/components/models/delete-server-modal";
 import { DeleteChannelModal } from "@/components/models/delete-channel-modal";
+import { EditChannelModal } from "@/components/models/edit-channel-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -31,6 +32,7 @@ export const ModalProvider = () => {
       <LeaveServerModal />
       <DeleteServerModal />
       <DeleteChannelModal />
+      <EditChannelModal />
     </>
   );
 };
