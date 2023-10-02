05d2a1ab9ff0061376162a0108924ecaaa14e228
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-10-02 15:51:37 +0000

    disable mobile sidebar fr

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -17,7 +17,7 @@ export const ChatHeader = ({
 }: ChatHeaderProps) => {
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-      <MobileToggle serverId={serverId} />
+      {/*<MobileToggle serverId={serverId} />*/}
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
       )}
