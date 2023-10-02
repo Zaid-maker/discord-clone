f4b6796516734bbffe3cdafcc5bd90514656c685
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-10-02 16:21:59 +0000

    added back support for moble toggle

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,5 +1,6 @@
 import { Hash } from "lucide-react";
 import { UserAvatar } from "@/components/user-avatar";
+import { MobileToggle } from "@/components/mobile-toggle";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -16,7 +17,7 @@ export const ChatHeader = ({
 }: ChatHeaderProps) => {
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-      {/*<MobileToggle serverId={serverId} />*/}
+      <MobileToggle serverId={serverId} />
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
       )}
