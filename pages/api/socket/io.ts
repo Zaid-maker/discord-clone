d5df2921696644a911c063b19f54a0c529ad727f
Zaid-maker <pzhafeez@gmail.com>
2023-10-13 20:07:12 +0500

    add api for coscket

---
+++ pages/api/socket/io.ts
@@ -0,0 +1,25 @@
+import { Server as NetServer } from "http";
+import { NextApiRequest } from "next";
+import { Server as ServerID } from "socket.io";
+
+import { NextApiResponseServerIo } from "@/types";
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
+
+const isHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
+    if (!res.socket.server.io) {
+        const path = "/api/socket/io"
+        const httpServer: NetServer = res.socket.server as any
+        const io = new ServerID(httpServer, {
+            path: path,
+            addTrailingSlash: false
+        })
+        res.socket.server.io = io
+    }
+
+    res.end()
+}
--- types.ts
+++ types.ts
@@ -1,5 +1,16 @@
 import { Member, Profile, Server } from "@prisma/client";
+import { Server as NetServer, Socket } from "net";
+import { NextApiRequest } from "next";
+import { Server as SocketIOServer } from "socket.io";
 
 export type ServerWithMembersWithProfiles = Server & {
   members: (Member & { profile: Profile })[];
+};
+
+export type NextApiResponseServerIo = NextApiRequest & {
+  socket: Socket & {
+    server: NetServer & {
+      io: SocketIOServer
+    }
+  }
 };
