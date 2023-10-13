64436844591da14626587b3975cf3425ea2ab97c
Zaid-maker <pzhafeez@gmail.com>
2023-10-13 21:04:06 +0500

    wrap socketProvider in main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { ModalProvider } from "@/components/providers/model-provider";
 import { cn } from "@/lib/utils";
 
 import "./globals.css";
+import { SocketProvider } from "@/components/providers/socket-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -30,8 +31,10 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
-            <ModalProvider />
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
