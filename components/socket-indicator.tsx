5999884e15d118c928f71cf99616a562becc8c93
Zaid-maker <pzhafeez@gmail.com>
2023-10-13 21:26:54 +0500

    create SocketIndicator component

---
+++ components/socket-indicator.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import { useSocket } from "@/components/providers/socket-provider";
+import { Badge } from "./ui/badge";
+
+export const SocketIndicator = () => {
+  const { isConnected } = useSocket();
+
+  if (!isConnected) {
+    return (
+      <Badge variant="outline" className="bg-yellow-600 text-white border-none">
+        Fallback: Falling every 1s
+      </Badge>
+    );
+  }
+
+  return (
+    <Badge variant="outline" className="bg-emerald-600 text-white border-none">
+      Live: Real-time updates
+    </Badge>
+  );
+};
