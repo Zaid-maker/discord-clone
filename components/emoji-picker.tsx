2b82e1e743bab4ca15929647235fd74f757f73ca
Zaid-maker <pzhafeez@gmail.com>
2023-10-18 21:24:54 +0500

    update EmojiPicker

--- components/emoji-picker.tsx
+++ components/emoji-picker.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+"use client";
 
-export const EmojiPicker = () => {
-  return <div>EmojiPicker</div>;
+import React from "react";
+import Picker from "@emoji-mart/react";
+import data from "@emoji-mart/data";
+import { Popover, PopoverTrigger } from "@/components/ui/popover";
+import { Smile } from "lucide-react";
+
+interface EmojiPickerProps {
+  onChange: (value: string) => void;
+}
+
+export const EmojiPicker = ({ onChange }: EmojiPickerProps) => {
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Smile className="text-zinc-500 dark:text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300 transition" />
+      </PopoverTrigger>
+    </Popover>
+  );
 };
