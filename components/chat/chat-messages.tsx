73e29842b4e8fd95e31b0ed1e140d59e7637457e
Zaid-maker <pzhafeez@gmail.com>
2023-10-22 20:38:11 +0500

    add status check

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -4,6 +4,7 @@ import React, { ElementRef, useRef } from "react";
 import { ChatWelcome } from "./chat-welcome";
 import { Member } from "@prisma/client";
 import { useChatQuery } from "@/hooks/use-chat-query";
+import { Loader2, ServerCrash } from "lucide-react";
 
 interface ChatMessagesProps {
   name: string;
@@ -42,6 +43,28 @@ export const ChatMessages = ({
       paramKey,
       paramValue,
     });
+
+  if (status === "loading") {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Loading Messages...
+        </p>
+      </div>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Something went wrong!
+        </p>
+      </div>
+    );
+  }
 
   return (
     <div ref={chatRef} className="flex-1 flex flex-col py-4 overflow-y-auto">
