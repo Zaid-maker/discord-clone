667f88b8c15cffdbe1e362ef0ad02eb6dfaa1528
DevMirza <pzhafeez@gmail.com>
2023-11-18 17:40:57 +0000

    style the chat
    undefined

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -2,6 +2,7 @@
 
 import { Member, Profile } from "@prisma/client";
 import React from "react";
+import { UserAvatar } from "../user-avatar";
 
 interface ChatItemProps {
   id: string;
@@ -30,5 +31,22 @@ export const ChatItem = ({
   socketUrl,
   socketQuery,
 }: ChatItemProps) => {
-  return <div>ChatItem</div>;
+  return (
+    <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
+      <div className="group flex gap-x-2 items-start w-full">
+        <div className="cursor-pointer hover:drop-shadow-md transition">
+          <UserAvatar src={member.profile.imageUrl} />
+        </div>
+        <div className="flex flex-col w-full">
+          <div className="flex items-center gap-x-2">
+            <div className="flex items-center">
+              <p className="font-semibold text-sm hover:underline cursor-pointer">
+                {member.profile.name}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
